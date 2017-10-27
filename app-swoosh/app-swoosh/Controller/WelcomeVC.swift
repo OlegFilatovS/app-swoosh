1ffb20acc27ce1d2c2249a91f3cb2a1001e0103f
Филатов Олег <joincs@yandex.ru>
2017-10-28 00:04:06 +0300

    many cool staff up here

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -13,9 +13,6 @@ class WelcomeVC: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
 
-
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
