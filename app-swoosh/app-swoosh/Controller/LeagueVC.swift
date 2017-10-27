abab72781f65d85c5d8c6af398b3b6e99b1717da
glebwell <gleb.maltcev@yandex.ru>
2017-10-27 20:43:24 +0300

    minor changes

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
 
 
-
-
-
-
+    @IBOutlet weak var nextBtn: BorderButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
@@ -28,6 +28,33 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skilVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+
+        selectLeague(leagueType: "mens")
+
+
+    }
+
+    @IBAction func onWomensTapped(_ sender: Any) {
+
+        selectLeague(leagueType: "womens")
+
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+
+        selectLeague(leagueType: "coed")
+
+    }
+
+
+    func selectLeague(leagueType: String) {
+
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+        nextBtn.setTitleColor(UIColor.white, for: .normal)
+
+    }
 
     /*
     // MARK: - Navigation
